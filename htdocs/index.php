30034df3b56333d91866354d3f0fd1bb1d98fe0f
Daniel Mikusa <dmikusa@gopivotal.com>
2013-07-26 16:36:19 -0400

    Referenced ci in it's non-public folder

--- htdocs/index.php
+++ htdocs/index.php
@@ -56,7 +56,7 @@ if (defined('ENVIRONMENT'))
  * as this file.
  *
  */
-	$system_path = 'system';
+	$system_path = $ENV['HOME'] . '/ci/system';
 
 /*
  *---------------------------------------------------------------
@@ -72,7 +72,7 @@ if (defined('ENVIRONMENT'))
  * NO TRAILING SLASH!
  *
  */
-	$application_folder = 'application';
+	$application_folder = $ENV['HOME'] . '/ci/application';
 
 /*
  * --------------------------------------------------------------------
