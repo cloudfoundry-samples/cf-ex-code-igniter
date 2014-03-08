4449d279ddee0901dc9e1873b9714782392397e1
Daniel Mikusa <dmikusa@gopivotal.com>
2014-03-08 18:59:16 -0500

    Added database code from tutorial.  Configured database credentials to come from the VCAP_SERVICES environment variable.

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,8 +38,10 @@
 |
 */
 
-$route['default_controller'] = 'pages/view';
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
+$route['default_controller'] = 'pages/view';
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
---
+++ application/controllers/news.php
@@ -0,0 +1,36 @@
+<?php
+class News extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('news_model');
+	}
+
+    public function index()
+    {
+        $data['news'] = $this->news_model->get_news();
+        $data['title'] = 'News archive';
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('news/index', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function view($slug)
+    {
+        $data['news_item'] = $this->news_model->get_news($slug);
+
+        if (empty($data['news_item']))
+        {
+            show_404();
+        }
+
+        $data['title'] = $data['news_item']['title'];
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('news/view', $data);
+        $this->load->view('templates/footer');
+    }
+}
+?>
