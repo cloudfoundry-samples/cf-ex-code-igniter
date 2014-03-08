5ec403915c8807eebcb7acd27dea3bef97d62c90
Daniel Mikusa <dmikusa@gopivotal.com>
2014-03-08 13:53:42 -0500

    Made CodeIgniter install directory easily configurable.

--- .extensions/code-igniter/extension.py
+++ .extensions/code-igniter/extension.py
@@ -36,7 +36,8 @@ def compile(install):
     print 'Installing Code Igniter %s' % DEFAULTS['CODEIGNITER_VERSION']
     ctx = install.builder._ctx
     inst = install._installer
-    workDir = os.path.join(ctx['TMPDIR'], 'code-igniter')
+    ciDir = 'codeigniter'
+    workDir = os.path.join(ctx['TMPDIR'], ciDir)
     inst.install_binary_direct(
         DEFAULTS['CODEIGNITER_URL'],
         DEFAULTS['CODEIGNITER_HASH'],
@@ -47,19 +48,21 @@ def compile(install):
         .everything()
         .where_name_matches('^%s.*$' % os.path.join(workDir, 'system'))
         .under(workDir)
-        .into('{BUILD_DIR}/code-igniter')
+        .into('{BUILD_DIR}/%s' % ciDir)
         .done())
     (install.builder
         .move()
         .everything()
         .where_name_matches('^%s.*$' % os.path.join(workDir, 'application'))
         .under(workDir)
-        .into('{BUILD_DIR}/code-igniter')
+        .into('{BUILD_DIR}/%s' % ciDir)
         .done())
     userAppFolder = os.path.join(ctx['BUILD_DIR'], 'htdocs', 'application')
     (install.builder
         .move()
         .everything()
+        .under('{BUILD_DIR}/htdocs')
         .where_name_matches('^%s.*$' % userAppFolder)
-        .under('{BUILD_DIR}/application'))
+        .into('{BUILD_DIR}/%s' % ciDir)
+        .done())
     return 0
